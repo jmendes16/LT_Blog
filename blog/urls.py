f8a96c9251d995afc6f2ab9fc79c8c89f10d6f1e
Joel Braganza-Mendes <113289625+jmendes16@users.noreply.github.com>
2022-12-04 00:25:18 +0000

    Update to previous push
    made numberic blog post calls function correctly

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:post>', views.blog_post, name='entries'),
-    path('<int:post_index>', views.blog_post_by_index, name='entries_index')
+    path('<int:post_index>', views.blog_post_by_index, name='entries_index'),
+    path('<str:post>', views.blog_post, name='entries')    
 ]
